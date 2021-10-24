2f15097743601d63c915a964aa38c9886abeccb9
@kadirsahan <kadir.sahan@gmail.com>
2021-10-24 11:30:31 +0300

    second commit

--- restApiAgent.py
+++ restApiAgent.py
@@ -4,13 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return "<h1>Hello World</h1>"
+  return "<h1>Dynamicfunctionreation</h1>"
 
 
 
 @app.route("/query")
 def index():
-  return "<h1>Hello World</h1>"
+  return "<h1>Dynamicfunctionreation</h1>"
+
+
 
 
 app.run(host="0.0.0.0", port=9080, debug=False)
